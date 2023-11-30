87de4e1d5ffe83edb9339379ee1895a7419ab134
Kristýna Machálková <macha@DESKTOP-TMSSHNL>
2023-11-30 21:05:25 +0100

    verze 1_2

--- Ukol7Eshop/Nahrdelnik.cs
+++ Ukol7Eshop/Nahrdelnik.cs
@@ -10,22 +10,8 @@ namespace Ukol7Eshop
     {
         public int DelkaNahrdelniku;
 
-        public Nahrdelnik(string druhSperku, string sku, string kov, string ryzostKovu, string barvaKovu, string kamen, double hmotnostGramy, double cena, string obrazek, string cestaKObrazku, bool jeSkladem, string nazev, string popis, int pocetKs, int delkaNahrdelniku)
+        public Nahrdelnik(string sku, string kov, string ryzostKovu, string barvaKovu, string kamen, double hmotnostGramy, double cena, string obrazek, string cestaKObrazku, bool jeSkladem, string nazev, string popis, int pocetKs, int delkaNahrdelniku) : base("Nahrdelnik", sku, kov, ryzostKovu, barvaKovu, kamen, hmotnostGramy, cena, obrazek, cestaKObrazku, jeSkladem, nazev, popis, pocetKs)
         {
-            DruhSperku = druhSperku;
-            Sku = sku;
-            Kov = kov;
-            RyzostKovu = ryzostKovu;
-            BarvaKovu = barvaKovu;
-            Kamen = kamen;
-            HmotnostGramy = hmotnostGramy;
-            Cena = cena;
-            Obrazek = obrazek;
-            CestaKObrazku = cestaKObrazku;
-            JeSkladem = jeSkladem;
-            Nazev = nazev;
-            Popis = popis;
-            PocetKs = pocetKs;
             DelkaNahrdelniku = delkaNahrdelniku;
         }
     }
--- Ukol7Eshop/Prsten.cs
+++ Ukol7Eshop/Prsten.cs
@@ -10,22 +10,8 @@ namespace Ukol7Eshop
     {
         public int VelikostPrstenu;
 
-        public Prsten(string druhSperku, string sku, string kov, string ryzostKovu, string barvaKovu, string kamen, double hmotnostGramy, double cena, string obrazek, string cestaKObrazku, bool jeSkladem, string nazev, string popis, int pocetKs, int velikostPrstenu)
+        public Prsten(string sku, string kov, string ryzostKovu, string barvaKovu, string kamen, double hmotnostGramy, double cena, string obrazek, string cestaKObrazku, bool jeSkladem, string nazev, string popis, int pocetKs, int velikostPrstenu) : base("Prsten", sku, kov, ryzostKovu, barvaKovu, kamen, hmotnostGramy, cena, obrazek, cestaKObrazku, jeSkladem, nazev, popis, pocetKs)
         {
-            DruhSperku = druhSperku;
-            Sku = sku;
-            Kov = kov;
-            RyzostKovu = ryzostKovu;
-            BarvaKovu = barvaKovu;
-            Kamen = kamen;
-            HmotnostGramy = hmotnostGramy;
-            Cena = cena;
-            Obrazek = obrazek;
-            CestaKObrazku = cestaKObrazku;
-            JeSkladem = jeSkladem;
-            Nazev = nazev;
-            Popis = popis;
-            PocetKs = pocetKs;
             VelikostPrstenu = velikostPrstenu;
         }
     }
